0a81f13bec1c2b0926be1f6fc564e0f3e6b75875
Halyna Bielova <halyna.bielova@chisw.com>
2024-10-29 13:33:59 +0200

    add fullscreen

--- presentation/src/main/java/com/example/presentation/screens/sharedcomponents/ExoPlayerView.kt
+++ presentation/src/main/java/com/example/presentation/screens/sharedcomponents/ExoPlayerView.kt
@@ -1,11 +1,19 @@
 package com.example.presentation.screens.sharedcomponents
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
@@ -13,6 +21,8 @@ import androidx.media3.ui.PlayerView
 @Composable
 fun ExoPlayerView(exoPlayer: ExoPlayer, videoUri: String, modifier: Modifier = Modifier) {
     val mediaItem = remember(videoUri) { MediaItem.fromUri(videoUri) }
+    val context = LocalContext.current
+
 
     LaunchedEffect(mediaItem) {
         exoPlayer.setMediaItem(mediaItem)
@@ -24,8 +34,58 @@ fun ExoPlayerView(exoPlayer: ExoPlayer, videoUri: String, modifier: Modifier = M
             exoPlayer.release()
         }
     }
+
     AndroidView(
-        factory = { ctx -> PlayerView(ctx).apply { player = exoPlayer } },
-        modifier = modifier
+        modifier = modifier,
+        factory = {
+            PlayerView(context).apply {
+                player = exoPlayer
+                setFullscreenButtonClickListener { isFullScreen ->
+                    with(context) {
+                        if (isFullScreen) {
+                            setScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+                        } else {
+                            setScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                        }
+                    }
+                }
+            }
+        }
     )
 }
+
+private fun Context.setScreenOrientation(orientation: Int) {
+    val activity = this.findActivity() ?: return
+    activity.requestedOrientation = orientation
+    if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+        hideSystemUi()
+    } else {
+        showSystemUi()
+    }
+}
+
+private fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+
+private fun Context.hideSystemUi() {
+    val window = findActivity()?.window ?: return
+    WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+}
+
+private fun Context.showSystemUi() {
+    val activity = this.findActivity() ?: return
+    val window = activity.window ?: return
+    WindowCompat.setDecorFitsSystemWindows(window, true)
+    WindowInsetsControllerCompat(
+        window,
+        window.decorView
+    ).show(WindowInsetsCompat.Type.systemBars())
+}
+
